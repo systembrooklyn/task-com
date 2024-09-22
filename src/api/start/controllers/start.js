832c8e38957e499751f37c6a5c37891105e2504a
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-22 16:39:18 +0300

    22-9-2024

--- src/api/start/controllers/start.js
+++ src/api/start/controllers/start.js
@@ -7,7 +7,8 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::start.start', ({ strapi }) => ({
-  
+
+  // وظيفة للتحقق من البريد الإلكتروني
   async checkEmail(ctx) {
     const { email } = ctx.query;
 
@@ -26,4 +27,45 @@ module.exports = createCoreController('api::start.start', ({ strapi }) => ({
 
     return ctx.send({ exists: false, message: 'البريد الإلكتروني غير مسجل.' });
   },
+
+  // وظيفة لجلب البيانات (find)
+  async find(ctx) {
+    const entities = await strapi.entityService.findMany('api::start.start', {
+      ...ctx.query,
+    });
+
+    return ctx.send({ data: entities });
+  },
+
+  // وظيفة لإنشاء بيانات جديدة (create)
+  async create(ctx) {
+    const { data } = ctx.request.body;  // الوصول إلى البيانات المرسلة في الطلب
+
+    if (!data) {
+      return ctx.badRequest('Data is required.');
+    }
+
+    const entity = await strapi.entityService.create('api::start.start', {
+      data: data,
+    });
+
+    return ctx.send({ data: entity });
+  },
+
+  // وظيفة لتحديث البيانات (update)
+  async update(ctx) {
+    const { id } = ctx.params;
+    const { data } = ctx.request.body;  // الوصول إلى البيانات المرسلة في الطلب
+
+    if (!data) {
+      return ctx.badRequest('Data is required.');
+    }
+
+    const updatedEntity = await strapi.entityService.update('api::start.start', id, {
+      data: data,
+    });
+
+    return ctx.send({ data: updatedEntity });
+  },
+
 }));
