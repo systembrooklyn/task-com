9519009b6b742cdea596b758d531b78ea891dc0c
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-22 15:40:14 +0300

    22-9-2024

--- src/api/start/controllers/start.js
+++ src/api/start/controllers/start.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::start.start');
+module.exports = createCoreController('api::start.start', ({ strapi }) => ({
+  
+  async checkEmail(ctx) {
+    const { email } = ctx.query;
+
+    if (!email) {
+      return ctx.badRequest('Email parameter is required.');
+    }
+
+    // التحقق مما إذا كان البريد الإلكتروني موجودًا
+    const existingEmail = await strapi.db.query('api::start.start').findOne({
+      where: { email: email }
+    });
+
+    if (existingEmail) {
+      return ctx.send({ exists: true, message: 'البريد الإلكتروني مسجل بالفعل.' });
+    }
+
+    return ctx.send({ exists: false, message: 'البريد الإلكتروني غير مسجل.' });
+  },
+}));
--- src/api/start/routes/start.js
+++ src/api/start/routes/start.js
@@ -7,3 +7,17 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::start.start');
+
+module.exports = {
+    routes: [
+      {
+        method: 'GET',
+        path: '/starts/check',  // المسار للتحقق من البريد الإلكتروني
+        handler: 'start.checkEmail',  // الوظيفة التي سيتم استدعاؤها
+        config: {
+          auth: false,  // يمكنك تفعيل أو تعطيل الصلاحيات هنا
+        },
+      },
+    ],
+  };
+  
