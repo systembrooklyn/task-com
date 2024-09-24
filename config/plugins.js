0f5d8d63a547b9d45a67cef359db807618cd8121
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-24 19:43:06 +0300

    24-9-2024

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          // You can configure additional options here
+        },
+        settings: {
+          defaultFrom: env('SMTP_USERNAME'),
+          defaultReplyTo: env('SMTP_USERNAME'),
+        },
+      },
+    },
+  });
+  
