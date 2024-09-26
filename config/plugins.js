c5271a3213933aa9bdcb42fd5ee44e1086af98d8
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-26 16:24:48 +0300

    26-9-2024

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,18 @@
 module.exports = ({ env }) => ({
-    email: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: 'smtp.gmail.com',  // خادم Gmail
-        port: 465,               // منفذ SSL الخاص بـ Gmail
-        auth: {
-          user: env('SMTP_USERNAME', 'system.bbs.2024@gmail.com'), // البريد الإلكتروني
-          pass: env('SMTP_PASSWORD', 'tvvieicmvmrmxsxr'),           // كلمة المرور
-        },
-        secure: true, // استخدم true لـ SSL (المنفذ 465)
-      },
-      settings: {
-        defaultFrom: 'system.bbs.2024@gmail.com', // البريد الذي سيتم الإرسال منه
-        defaultReplyTo: 'system.bbs.2024@gmail.com',
+  email: {
+    provider: 'smtp',
+    providerOptions: {
+      host: env('SMTP_HOST', 'mail.brooklynacademy.net'), // خادم SMTP الخاص بنطاقك
+      port: env('SMTP_PORT', 465), // المنفذ 465 لـ SSL
+      secure: true, // استخدام SSL
+      auth: {
+        user: env('SMTP_USERNAME', 'mytask@brooklynacademy.net'), // بريدك الإلكتروني
+        pass: env('SMTP_PASSWORD', 'U6}RqMX[,4gF'), // كلمة المرور الخاصة ببريدك الإلكتروني
       },
     },
-  });
-  
+    settings: {
+      defaultFrom: 'mytask@brooklynacademy.net', // البريد الإلكتروني الافتراضي للإرسال
+      defaultReplyTo: 'mytask@brooklynacademy.net', // البريد الإلكتروني الافتراضي للرد
+    },
+  },
+});
