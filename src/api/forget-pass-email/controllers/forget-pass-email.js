a14e1e2fc91e35062b202838348e24ced59755fc
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-29 16:57:54 +0300

    29-9-2024

--- src/api/forget-pass-email/controllers/forget-pass-email.js
+++ src/api/forget-pass-email/controllers/forget-pass-email.js
@@ -5,42 +5,24 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-const crypto = require('crypto');
 
-// إعدادات التشفير
-const secretKey = 'vZ0D8X$yE6Ng^9P3r@H#w1RLvC$Sx^5M'; // استخدم مفتاح سري قوي ويُفضل وضعه في ملف .env
-const algorithm = 'aes-256-cbc';
-
-// دالة لتشفير البريد الإلكتروني
-const encryptEmail = (email) => {
-  const iv = crypto.randomBytes(16); // إنشاء IV عشوائي
-  const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey, 'hex'), iv);
-  let encrypted = cipher.update(email);
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
-};
 
 module.exports = createCoreController("api::forget-pass-email.forget-pass-email", ({ strapi }) => ({
 
-  async create(ctx) {
-    const { data } = ctx.request.body; // الوصول إلى البيانات المرسلة في الطلب
+    async create(ctx) {
+        const { data } = ctx.request.body; // الوصول إلى البيانات المرسلة في الطلب
 
-    if (!data || !data.email) {
-      return ctx.badRequest("Email is required.");
-    }
-
-    // تشفير البريد الإلكتروني
-    const encryptedEmail = encryptEmail(data.email);
-
-    console.log(encryptedEmail);
-
-    // استخدام البريد الإلكتروني المشفر للتخزين أو الإرسال
-    data.email = encryptedEmail;
-
-    const entity = await strapi.entityService.create("api::forget-pass-email.forget-pass-email", {
-      data: data,
-    });
-
-    return ctx.send({ data: entity });
-  },
+        console.log(data);
+    
+        if (!data) {
+          return ctx.badRequest("Data is required.");
+        }
+    
+        const entity = await strapi.entityService.create("api::forget-pass-email.forget-pass-email", {
+          data: data,
+        });
+        
+        return ctx.send({ data: entity });
+      },
 }));
+
