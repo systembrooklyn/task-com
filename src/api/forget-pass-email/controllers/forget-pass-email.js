c3f5a953bdc1d322ed879dd7dc597d4a05eef914
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-29 16:01:42 +0300

    29-9-2024

--- src/api/forget-pass-email/controllers/forget-pass-email.js
+++ src/api/forget-pass-email/controllers/forget-pass-email.js
@@ -6,21 +6,31 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
+// تعريف دالة التشفير هنا
+const encodeEmail = (email) => {
+  return Buffer.from(email).toString('base64');
+};
 
 module.exports = createCoreController("api::forget-pass-email.forget-pass-email", ({ strapi }) => ({
 
-    async create(ctx) {
-        const { data } = ctx.request.body; // الوصول إلى البيانات المرسلة في الطلب
-    
-        if (!data) {
-          return ctx.badRequest("Data is required.");
-        }
-    
-        const entity = await strapi.entityService.create("api::forget-pass-email.forget-pass-email", {
-          data: data,
-        });
-    
-        return ctx.send({ data: entity });
-      },
-}));
+  async create(ctx) {
+    const { data } = ctx.request.body; // الوصول إلى البيانات المرسلة في الطلب
 
+    if (!data || !data.email) {
+      return ctx.badRequest("Email is required.");
+    }
+
+    // تشفير البريد الإلكتروني
+    const encodedEmail = encodeEmail(data.email);
+    console.log(encodedEmail);
+
+    // استخدام البريد الإلكتروني المشفر للتخزين أو الإرسال
+    data.email = encodedEmail;
+
+    const entity = await strapi.entityService.create("api::forget-pass-email.forget-pass-email", {
+      data: data,
+    });
+
+    return ctx.send({ data: entity });
+  },
+}));
