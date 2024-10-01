9e07bf87ef5a484fb3be39cb60f994204320b8a8
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-10-01 17:53:19 +0300

    1-10-2024

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -447,7 +447,7 @@ export interface ApiStartStart extends Schema.CollectionType {
     password: Attribute.String;
     companyId: Attribute.BigInteger;
     role: Attribute.String;
-    name: Attribute.String & Attribute.Required;
+    name: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
