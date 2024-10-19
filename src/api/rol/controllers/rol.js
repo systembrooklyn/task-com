af8a261e061413aa3fc891cc865c23ed381a0fbd
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-10-19 18:47:25 +0300

    update create fun in rols

--- src/api/rol/controllers/rol.js
+++ src/api/rol/controllers/rol.js
@@ -12,33 +12,28 @@ module.exports = createCoreController('api::rol.rol', ({ strapi }) => ({
 
   async create(ctx) {
     try {
-      // استلام البيانات من الطلب
       const { name, companyId, permissions } = ctx.request.body.data;
 
-      // تحقق من أن الحقول المطلوبة موجودة
-      if (!name || !permissions || Object.keys(permissions).length === 0) {
+      if (!name || !permissions || permissions.length === 0) {
         return ctx.badRequest('Role name and permissions are required.');
       }
 
-      // تأكد من أن الصلاحيات يتم حفظها كـ JSON أو بالشكل الصحيح في قاعدة البيانات
+      // تأكد من أن permissions تحتوي على معرفات الصلاحيات الصحيحة
       const createdRole = await strapi.entityService.create('api::rol.rol', {
         data: {
           name,
-          companyId: companyId || null, // يمكن أن يكون null إذا لم يكن companyId موجودًا
-          permissions: permissions, // حفظ الصلاحيات ككائن (JSON)
+          companyId,
+          permissions, // تمرير معرفات الصلاحيات كعلاقة
         },
       });
-      
 
-      console.log('create role : ' + createdRole);
-
-      // إعادة الرد بنجاح مع البيانات التي تم إنشاؤها
       return { data: createdRole };
     } catch (error) {
-      console.error('Error creating role with permissions:', error);
+      console.error("Error creating role with permissions:", error);
       ctx.throw(500, 'An error occurred while creating the role');
     }
   },
+
 
   async find(ctx) {
     try {
