c69804bd17150c74fbd593acf2af9eb573757bc3
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-10-23 18:18:30 +0300

    make create & find fun in perms table

--- src/api/perm/controllers/perm.js
+++ src/api/perm/controllers/perm.js
@@ -1,9 +1,51 @@
-'use strict';
+"use strict";
 
 /**
  * perm controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::perm.perm');
+module.exports = createCoreController("api::perm.perm", ({ strapi }) => ({
+  // إنشاء صلاحيات جديدة
+  async create(ctx) {
+    const { data } = ctx.request.body;
+
+    try {
+      // إنشاء كائن جديد للصلاحيات بناءً على البيانات المستلمة
+      const newPerm = await strapi.entityService.create("api::perm.perm", {
+        data: {
+          canAddUser: data.canAddUser || false,
+          canEditUser: data.canEditUser || false,
+          canDeleteUser: data.canDeleteUser || false,
+        },
+      });
+
+      // إرجاع البيانات المحفوظة مع الـ ID الخاص بها
+      ctx.send({
+        id: newPerm.id,
+        message: "Permissions added successfully",
+        // جلب الصلاحيات المرتبطة
+        permissions: newPerm,
+      });
+    } catch (error) {
+      ctx.throw(400, "Error creating permissions");
+    }
+  },
+
+  // جلب جميع الصلاحيات
+  async find(ctx) {
+    try {
+      const perms = await strapi.entityService.findMany("api::perm.perm", {
+        populate: ["rols"], // إذا كنت ترغب في جلب الحقول المرتبطة بـ rols
+      });
+
+      ctx.send({
+        data: perms,
+        message: "Permissions found successfully",
+      });
+    } catch (error) {
+      ctx.throw(400, "Error fetching permissions");
+    }
+  },
+}));
--- src/api/perm/routes/perm.js
+++ src/api/perm/routes/perm.js
@@ -7,3 +7,24 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::perm.perm');
+
+module.exports = {
+  routes: [
+      {
+        "method": "GET",
+        "path": "/perms",
+        "handler": "perm.find",
+        "config": {
+          "policies": []
+        }
+      },
+      {
+        "method": "POST",
+        "path": "/perms",
+        "handler": "perm.create",
+        "config": {
+          "policies": []
+        }
+      },
+    ],
+};
