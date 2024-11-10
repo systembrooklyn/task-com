a05531eb90f7730f1cb5788f362cf5c68939cff6
Hazem Essam <133036561+Hazem1199@users.noreply.github.com>
2024-11-10 19:10:58 +0200

    add companyID to department create and update

--- src/api/department/controllers/department.js
+++ src/api/department/controllers/department.js
@@ -22,7 +22,7 @@ module.exports = createCoreController(
     },
     async create(ctx) {
       try {
-        const { departmentName, employeeIds } = ctx.request.body.data;
+        const { departmentName, employeeIds, companyID } = ctx.request.body.data;
 
         if (!departmentName) {
           return ctx.badRequest("Department name is required.");
