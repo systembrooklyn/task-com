67db48084f557572eb5d758f5044cbc982025a8a
Hazem Essam <133036561+Hazem1199@users.noreply.github.com>
2024-11-25 19:31:24 +0200

    ..

--- src/api/department/controllers/department.js
+++ src/api/department/controllers/department.js
@@ -6,84 +6,92 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::department.department", ({ strapi }) => ({
-  async find(ctx) {
-    const entities = await strapi.entityService.findMany(
-      "api::department.department",
-      {
-        ...ctx.query,
-        populate: {
-          employees: {
-            populate: {
-              position: true, // جلب بيانات المناصب المرتبطة بكل موظف
-            },
-          },
-        },
-      }
-    );
-    return { data: entities };
-  },
-
-  async create(ctx) {
-    try {
-      const { departmentName, employeeIds, companyID } = ctx.request.body.data;
-
-      if (!departmentName) {
-        return ctx.badRequest("Department name is required.");
-      }
-
-      const createdDepartment = await strapi.entityService.create(
+module.exports = createCoreController(
+  "api::department.department",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const entities = await strapi.entityService.findMany(
         "api::department.department",
         {
-          data: {
-            departmentName,
-            companyID,
-            employees: employeeIds,
+          ...ctx.query,
+          populate: {
+            employees: {
+              populate: {
+                position: true, // جلب بيانات المناصب المرتبطة بكل موظف
+              },
+            },
           },
         }
       );
+      return { data: entities };
+    },
 
-      const populatedDepartment = await strapi.entityService.findOne("api::department.department", createdDepartment.id, {
-        populate: {
-          employees: {
-            populate: {
-              position: true,
+    async create(ctx) {
+      try {
+        const { departmentName, employeeIds, companyID } =
+          ctx.request.body.data;
+
+        if (!departmentName) {
+          return ctx.badRequest("Department name is required.");
+        }
+
+        const createdDepartment = await strapi.entityService.create(
+          "api::department.department",
+          {
+            data: {
+              departmentName,
+              companyID,
+              employees: employeeIds,
             },
-          },
-        },
-      });
+          }
+        );
 
-      return { data: populatedDepartment };
-    } catch (error) {
-      console.error("Error creating department:", error);
-      ctx.throw(500, "An error occurred while creating the department");
-    }
-  },
-  async update(ctx) {
-    const { id } = ctx.params;
-    const { data } = ctx.request.body;
+        const populatedDepartment = await strapi.entityService.findOne(
+          "api::department.department",
+          createdDepartment.id,
+          {
+            populate: {
+              employees: {
+                populate: {
+                  position: true,
+                },
+              },
+            },
+          }
+        );
 
-    const updatedData = {
-      departmentName: data.departmentName,
-      employees: data.employees,
-    };
-
-    const updatedDepartment = await strapi.entityService.update(
-      "api::department.department",
-      id,
-      {
-        data: updatedData,
+        return { data: populatedDepartment };
+      } catch (error) {
+        console.error("Error creating department:", error);
+        ctx.throw(500, "An error occurred while creating the department");
       }
-    );
+    },
+    async update(ctx) {
+      const { id } = ctx.params;
+      const { data } = ctx.request.body;
 
-    return {
-      data: updatedDepartment,
-      message: "Department updated successfully",
-    };
-  },
-  async delete(ctx) {
-    const { id } = ctx.params;
-    await strapi.entityService.delete("api::department.department", id);
-    return { message: "Department deleted successfully" };
-  },
-}));
+      // const updatedData = {
+      //   departmentName: data.departmentName,
+      //   employees: data.employees,
+      // };
+
+      const updatedDepartment = await strapi.entityService.update(
+        "api::department.department",
+        id,
+        {
+          data: data,
+        }
+      );
+
+      return {
+        data: updatedDepartment,
+        message: "Department updated successfully",
+      };
+    },
+    async delete(ctx) {
+      const { id } = ctx.params;
+      await strapi.entityService.delete("api::department.department", id);
+      return { message: "Department deleted successfully" };
+    },
+  })
+);
